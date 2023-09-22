9364ae1fb3b3b84643ce9812b9bb00252b2d7901
pradeepkumarksp <mycomputer252545@gmail.com>
2023-09-22 12:44:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+let container = document.createElement('div');
+container.classList.add('container');
+let span = document.getElementById('span');
+span.innerHTML = 10;
+let count = 10;
+setTimeout(() =>{
+    count = count - 1;
+    span.innerHTML = count;
+    setTimeout(() =>{
+        count = count - 1;
+        span.innerHTML = count;
+        setTimeout(() =>{
+            count = count - 1;
+            span.innerHTML = count;
+            setTimeout(() => {
+                count = count - 1;
+                span.innerHTML = count;
+                setTimeout(() => {
+                    count = count - 1;
+                    span.innerHTML = count;
+                    setTimeout(() => {
+                        count = count - 1;
+                        span.innerHTML = count;
+                        setTimeout(() => {
+                            count = count - 1;
+                            span.innerHTML = count;
+                            setTimeout(() => {
+                                count = count - 1;
+                                span.innerHTML = count;
+                                setTimeout(() => {
+                                    count = count - 1;
+                                    span.innerHTML = count;
+                                    setTimeout(() => {
+                                        count = count - 1;
+                                        span.innerHTML = 'Happy Independence Day';
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        },1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}, 1000)
+
+
+document.body.append(container);
